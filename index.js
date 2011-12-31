e23016536af63c7bc4aafbcf2aa82aa41aab5f54
Alex Xu <alex_y_xu@yahoo.ca>
2011-12-31 11:21:15 -0500

    "proper" multi-line string

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
 /**
  * Happy new year Node.js people!
  */
-var text = [
-  '',
-  '',
-  'o      O    Oo    OooOOo.  OooOOo.  o       O       o.     O o.OOoOoo o          `O       o       O o.OOoOoo    Oo    `OooOOo.                                  ',
-  'O      o   o  O   O     `O O     `O O       o       Oo     o  O       O           o       O       o  O         o  O    o     `o       .oOOo. .oOOo.  oO   .oOOo.',
-  'o      O  O    o  o      O o      O `o     O\'       O O    O  o       o           O       `o     O\'  o        O    o   O      O            O O    o   O        O',
-  'OoOooOOo oOooOoOo O     .o O     .o   O   o         O  o   o  ooOO    O           O         O   o    ooOO    oOooOoOo  o     .O            o o    O   o        o',
-  'o      O o      O oOooOO\'  oOooOO\'     `O\'          O   o  O  O       o     o     o          `O\'     O       o      O  OOooOO\'            O\' o    o   O       O\'',
-  'O      o O      o o        o            o           o    O O  o       O     O     O           o      o       O      o  o    o            O   O    O   o      O  ',
-  'o      o o      O O        O            O           o     Oo  O       `o   O o   O\'           O      O       o      O  O     O         .O    o    O   O    .O  ',
-  'o      O O.     O o\'       o\'           O           O     `o ooOooOoO  `OoO\' `OoO\'            O     ooOooOoO O.     O  O      o       oOoOoO `OooO\' OooOO oOoOoO',
-  '',
-  '',
-  'Thank you for being such a great community!',
-  '',
-  '',
-];
-
-text.forEach(function(line) {
-  console.log(line);
-});
+var text = "\n\
+o      O    Oo    OooOOo.  OooOOo.  o       O       o.     O o.OOoOoo o          `O       o       O o.OOoOoo    Oo    `OooOOo.                                  \n\
+O      o   o  O   O     `O O     `O O       o       Oo     o  O       O           o       O       o  O         o  O    o     `o       .oOOo. .oOOo.  oO   .oOOo.\n\
+o      O  O    o  o      O o      O `o     O'       O O    O  o       o           O       `o     O'  o        O    o   O      O            O O    o   O        O\n\
+OoOooOOo oOooOoOo O     .o O     .o   O   o         O  o   o  ooOO    O           O         O   o    ooOO    oOooOoOo  o     .O            o o    O   o        o\n\
+o      O o      O oOooOO'  oOooOO'     `O'          O   o  O  O       o     o     o          `O'     O       o      O  OOooOO'            O' o    o   O       O'\n\
+O      o O      o o        o            o           o    O O  o       O     O     O           o      o       O      o  o    o            O   O    O   o      O\n\
+o      o o      O O        O            O           o     Oo  O       `o   O o   O'           O      O       o      O  O     O         .O    o    O   O    .O\n\
+o      O O.     O o'       o'           O           O     `o ooOooOoO  `OoO' `OoO'            O     ooOooOoO O.     O  O      o       oOoOoO `OooO' OooOO oOoOoO\n\
+\n\
+Thank you for being such a great community!\n\
+";
+console.log(text);
